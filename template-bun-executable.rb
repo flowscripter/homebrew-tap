7f72b39d4425821810ba74115ca180405e9c360e
vectronic <hello.vectronic@gmail.com>
2025-02-20 18:50:21 +0000

    fix: fix version

--- template-bun-executable.rb
+++ template-bun-executable.rb
@@ -1,7 +1,7 @@
 class TemplateBunExecutable < Formula
   desc "Project template for a cross-platform Bun executable with ffi native library and Bun library dependencies."
   homepage "https://github.com/flowscripter/template-bun-executable"
-  url "https://github.com/flowscripter/template-bun-executable/releases/download/vv1.1.3/template-bun-executable_MacOS_aarch64.zip"
+  url "https://github.com/flowscripter/template-bun-executable/releases/download/v1.1.3/template-bun-executable_MacOS_aarch64.zip"
   sha256 "36ca36ba59322bf53356ecd640b36f4457cb14d1071225e71a71ffc7645fe7b0"
   license "MIT"
   version "v1.1.3"
