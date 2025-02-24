0295027dfb569760f43138e2625ff398a802d966
vectronic <hello.vectronic@gmail.com>
2025-02-24 01:13:15 +0000

    Update Brew formula for template-bun-executable to v1.1.7

--- template-bun-executable.rb
+++ template-bun-executable.rb
@@ -1,10 +1,10 @@
 class TemplateBunExecutable < Formula
   desc "Project template for a cross-platform Bun executable with ffi native library and Bun library dependencies."
   homepage "https://github.com/flowscripter/template-bun-executable"
-  url "https://github.com/flowscripter/template-bun-executable/releases/download/v1.1.6/template-bun-executable_MacOS_aarch64.zip"
-  sha256 "0f8ad407cd1ea65115dfa26ed57ecc6396faf259c1b5ee2cbde7afb5b6be9beb"
+  url "https://github.com/flowscripter/template-bun-executable/releases/download/v1.1.7/template-bun-executable_MacOS_aarch64.zip"
+  sha256 "9c825fe29fc5071e276939a479b53eec056d34ba3986351331b93855b588a724"
   license "MIT"
-  version "v1.1.6"
+  version "v1.1.7"
 
   def install
     bin.install "template-bun-executable"
