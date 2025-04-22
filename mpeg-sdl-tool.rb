37806ae3ef2782808c27a6b8fe0390ff4506b347
vectronic <hello.vectronic@gmail.com>
2025-04-22 19:25:10 +0100

    Update Brew formula for mpeg-sdl-tool to v1.0.4

--- mpeg-sdl-tool.rb
+++ mpeg-sdl-tool.rb
@@ -1,10 +1,10 @@
 class MpegSdlTool < Formula
   desc "ISO/IEC 14496-34 Syntactic Description Language (MPEG SDL) CLI tool"
   homepage "https://github.com/flowscripter/mpeg-sdl-tool"
-  url "https://github.com/flowscripter/mpeg-sdl-tool/releases/download/v1.0.3/mpeg-sdl-tool_MacOS_aarch64.zip"
-  sha256 "fd040f5ceb7aeca8e6b7fd7c869e965bd861a041ba250a711ae643938ed9463b"
+  url "https://github.com/flowscripter/mpeg-sdl-tool/releases/download/v1.0.4/mpeg-sdl-tool_MacOS_aarch64.zip"
+  sha256 "62cb64b68fdb5009c56654ca3601eb3875892c5535635a232b235736f78de777"
   license "MIT"
-  version "v1.0.3"
+  version "v1.0.4"
 
   def install
     bin.install "mpeg-sdl-tool"
