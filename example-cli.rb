07c25d6522b4a7bae7c443ada93b065e9e585825
vectronic <hello.vectronic@gmail.com>
2026-06-09 15:40:52 +0100

    fix: correct download URLs for example-cli and example-host-application

--- example-cli.rb
+++ example-cli.rb
@@ -1,7 +1,7 @@
 class ExampleCli < Formula
   desc "Simple example CLI using https://github.com/flowscripter/dynamic-cli-framework"
   homepage "https://github.com/flowscripter/example-cli"
-  url "https://github.com/flowscripter/example-cli/releases/download//example-cli_MacOS_aarch64.zip"
+  url "https://github.com/flowscripter/example-cli/releases/download/v1.3.1/example-cli_MacOS_aarch64.zip"
   sha256 "d6c65ed442b52b260ed6ba11e839a278972f20c3fce7db20b67054fcdd551904"
   license "MIT"
   version "v1.3.1"
--- example-host-application.rb
+++ example-host-application.rb
@@ -1,7 +1,7 @@
 class ExampleHostApplication < Formula
   desc "Example host application executable for the https://github.com/flowscripter/dynamic-plugin-framework"
   homepage "https://github.com/flowscripter/example-host-application"
-  url "https://github.com/flowscripter/example-host-application/releases/download//example-host-application_MacOS_aarch64.zip"
+  url "https://github.com/flowscripter/example-host-application/releases/download/v1.2.0/example-host-application_MacOS_aarch64.zip"
   sha256 "c7aee5e757f02863fdf2e7c3ebdf7135e0f7c96bb61313c361fbcc615013a380"
   license "MIT"
   version "v1.2.0"
