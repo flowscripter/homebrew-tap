0e9dedbe7785f60bf7e2fbaa41d9cac796951ded
Nick Ryan <hello.vectronic@gmail.com>
2026-06-17 03:40:40 +0100

    Update Brew formula for example-host-application to v1.2.9

--- example-host-application.rb
+++ example-host-application.rb
@@ -1,10 +1,10 @@
 class ExampleHostApplication < Formula
   desc "Example host application executable for the https://github.com/flowscripter/dynamic-plugin-framework"
   homepage "https://github.com/flowscripter/example-host-application"
-  url "https://github.com/flowscripter/example-host-application/releases/download/v1.2.8/example-host-application_MacOS_aarch64.zip"
-  sha256 "94f1160f766294ada1586dcc0c71896dec78fc0ae9d3410d2ce029878c985a88"
+  url "https://github.com/flowscripter/example-host-application/releases/download/v1.2.9/example-host-application_MacOS_aarch64.zip"
+  sha256 "59c86bc1406419fd33c08180e60855dbe4e3176052dc7be9479eadd103ccfa89"
   license "MIT"
-  version "v1.2.8"
+  version "v1.2.9"
 
   def install
     bin.install "example-host-application"
