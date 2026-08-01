539c64293f562f031bf9e2829bc48639d40114a0
Nick Ryan <hello.vectronic@gmail.com>
2026-08-01 15:24:43 +1000

    Update Brew formula for example-cli to v1.8.3

--- example-cli.rb
+++ example-cli.rb
@@ -1,10 +1,10 @@
 class ExampleCli < Formula
   desc "Simple example CLI using https://github.com/flowscripter/dynamic-cli-framework"
   homepage "https://github.com/flowscripter/example-cli"
-  url "https://github.com/flowscripter/example-cli/releases/download/v1.8.2/example-cli_MacOS_aarch64.zip"
-  sha256 "1c226b47c7ac78127bed0f29ce22c8bb281e308d81b30602173a9f2180a6413f"
+  url "https://github.com/flowscripter/example-cli/releases/download/v1.8.3/example-cli_MacOS_aarch64.zip"
+  sha256 "dca48678607a2d65f3756861667d0bb24acb5447ceb8a5661938c51c9ff827b9"
   license "MIT"
-  version "v1.8.2"
+  version "v1.8.3"
 
   def install
     bin.install "example-cli"
