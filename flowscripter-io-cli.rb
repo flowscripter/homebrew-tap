4ca22e2b4d6d4a9f6738463a14ec29e48c0f886c
Nick Ryan <hello.vectronic@gmail.com>
2026-09-09 18:22:54 +0100

    Update Brew formula for flowscripter-io-cli to v1.2.2

--- flowscripter-io-cli.rb
+++ flowscripter-io-cli.rb
@@ -1,10 +1,10 @@
 class FlowscripterIoCli < Formula
   desc "Example CLI using https://github.com/flowscripter/pluggable-io-framework"
   homepage "https://github.com/flowscripter/flowscripter-io-cli"
-  url "https://github.com/flowscripter/flowscripter-io-cli/releases/download/v1.2.1/flowscripter-io-cli_MacOS_aarch64.zip"
-  sha256 "7553c1cab473edae1791f9ba5d440f1fb19e52577371467a00be3a9c2bbf7f22"
+  url "https://github.com/flowscripter/flowscripter-io-cli/releases/download/v1.2.2/flowscripter-io-cli_MacOS_aarch64.zip"
+  sha256 "a174f9360d66a41830d3d37c39f354df80ad72f6fd28ead3ed070eda386b3c5e"
   license "MIT"
-  version "v1.2.1"
+  version "v1.2.2"
 
   def install
     bin.install "flowscripter-io-cli"
